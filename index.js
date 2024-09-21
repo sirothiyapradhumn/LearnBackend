afaa2c7fa543a9789a956988e9f1eb1315b8d274
Pradhumn <sirothiyapradhumn@gmail.com>
2024-09-21 16:04:24 +0530

    CURD API

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const products = data.products;
 const express = require('express');
 const server = express();
 
+// Inbuild middleware -> to get info from body
+server.use(express.json())
+
 // Custom middleware -> logger - on server level
 server.use((req, res, next) => {
   console.log(req.method, req.ip, req.hostname);
@@ -25,8 +28,57 @@ const auth = ((req, res, next) => {
 
 // API -> endPoints
 
+// REST API 
+// C U R D API
+
+// Create
+server.post('/product', (req, res) => {
+  const data = req.body;
+  products.push(data);
+  res.json(data);
+});
+
+//Update
+server.put('/product/:id', (req, res) => {
+  const productId = +req.params.id;
+  const data = req.body;
+  const findProductIdx = products.findIndex((item) => item.id === productId);
+  products.splice(findProductIdx, 1, {...data, id: productId});
+  res.json(data);
+});
+
+server.patch('/product/:id', (req, res) => {
+  const productId = +req.params.id;
+  const data = req.body;
+  const findProductIdx = products.findIndex((item) => item.id === productId);
+  products.splice(findProductIdx, 1, {...products[findProductIdx], ...data});
+  res.json(data);
+});
+
+
+// Read
+server.get('/product', (req, res) => {
+  res.json(products);
+});
+
 server.get('/product/:id', (req, res) => {
-  console.log(req.params);
+  const productId = +req.params.id;
+  const findProduct = products.find((item) => item.id === productId);
+  res.json(findProduct);
+});
+
+//Delete
+server.delete('/product/:id', (req, res) => {
+  const productId = +req.params.id;
+  const findProductIdx = products.findIndex((item) => item.id === productId);
+  const showProduct = products[findProductIdx];
+  products.splice(findProductIdx, 1);
+  res.json(showProduct);
+});
+  
+
+// --------------------------------------------------------------------------------------
+server.get('/', auth, (req, res) => {
   res.json({type: 'GET'})
 })
 
@@ -49,8 +101,6 @@ server.patch('/', (req, res) => {
 server.delete('/', (req, res) => {
   res.json({type: 'DELETE'})
 })
-
-
 
 
 
