aceb06e9cc613f206a2d405de140ffdbbe3447b2
sirothiyapradhumn <sirothiyapradhumn@gmail.com>
2024-09-29 15:41:55 +0530

    connection to DB

---
+++ BEBasicLearn/connectDB.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://sirothiyasahilipad:aJnOVSYixa1gzxQ8@clustert0.cdwmq.mongodb.net/userappnew",
+);
+
+const userModal = mongoose.model("Users", {
+  name: String,
+  email: String,
+  password: String,
+});
+
+app.post("/signUp", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  console.log('userBody',  name, email, password );
+
+  const userExist = await userModal.findOne({ email });
+  console.log('userExist', userExist);
+
+  if (userExist) {
+    res.status(400).send("User already exist");
+  }
+
+  const user = new userModal({
+    name,
+    email,
+    password,
+  });
+
+  console.log("user", user);
+
+  await user.save();
+  res.json({
+    msg: "User created successfully",
+    user: {
+      name,
+      email,
+    }
+  });
+});
+
+app.listen(8080, () => {
+  console.log(`Example app listening at http://localhost:8080`);
+});
